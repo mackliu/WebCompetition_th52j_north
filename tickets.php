256a639f554a1d60323396044791cf8fe9fb7a51
mackliu <macklun@gmail.comm>
2022-03-06 13:27:13 +0800

    精簡CSS及排版方式,輪播程式碼再精簡

--- tickets.php
+++ tickets.php
@@ -1,13 +1,17 @@
 <?php
+//建立連線資料庫所需資訊，DB命名請依競賽當天公告規定
 $dsn="mysql:host=localhost;charset=utf8;dbname=db01";
+
+//建立PDO連線物件
 $pdo=new PDO($dsn,'root','mack1007');
 
+//建立SQL新增資料語法
 $sql="INSERT INTO `tickets` (`first_name`,`last_name`,`phone`,`pw`) VALUES('{$_POST['first_name']}','{$_POST['last_name']}','{$_POST['phone']}','{$_POST['pw']}')";
 
+//連線資料庫並執行SQL語法完成新增功能
 $pdo->exec($sql);
-echo $sql;
-//header("location:home.html");
 
-
+//將頁面導向首頁
+header("location:home.html");
 
 ?>
